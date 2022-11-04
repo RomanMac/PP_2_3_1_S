ac5b23499d8673175a40966b9968553952de8003
Роман <mackarow.romik@yandex.ru>
2022-11-04 15:34:17 +0300

    11

--- src/main/Java/web/controller/UserController.java
+++ src/main/Java/web/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
+import web.service.UserService;
 import web.service.UserServiceImpl;
 
 import javax.validation.Valid;
@@ -14,7 +15,7 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    UserServiceImpl userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserServiceImpl userService) {
